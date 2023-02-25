0bc504b37ed1d87e785600bfcee6411ec4535f0e
knightriders6099 <knightriders6099@gmail.com>
2023-02-25 13:56:09 -0500

    Implement leveler with updated code for IMU

--- src/main/java/frc/robot/subsystems/RobotLeveler.java
+++ src/main/java/frc/robot/subsystems/RobotLeveler.java
@@ -4,17 +4,24 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.model.LevelMovement;
+import frc.robot.subsystems.thirdparty.ADIS16470_IMU;
+import frc.robot.subsystems.thirdparty.ADIS16470_IMU.IMUAxis;
 
 public class RobotLeveler extends SubsystemBase {
 
   private final ADIS16470_IMU imu;
+  private final IMUAxis yawAxis;
+  private final IMUAxis rollAxis;
+  private final IMUAxis pitchAxis;
   /** Creates a new RobotLeveler. */
   public RobotLeveler() {
     this.imu = new ADIS16470_IMU();
+    this.yawAxis = imu.getYawAxis();
+    this.rollAxis = imu.getRollAxis();
+    this.pitchAxis = imu.getPitchAxis();
   }
 
   @Override
@@ -22,16 +29,13 @@ public class RobotLeveler extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public boolean makeRobotLevel() {
-    // TODO: This needs to be heavily re-evaluated
+  public LevelMovement getLevelMovement() {
+    // TODO: This needs to be heavily evaluated
     //  https://www.chiefdelphi.com/t/adis-16470-wpilibj-class-mystery-why-can-we-only-get-one-axis-at-a-time/425461
     //  https://www.chiefdelphi.com/t/analog-devices-adis-16470-eng-basic-java-gyro-code-help/422553
-    this.imu.setYawAxis(IMUAxis.kX);
-    double yaw = this.imu.getAngle();
-    this.imu.setYawAxis(IMUAxis.kZ);
-    double roll = this.imu.getAngle();
-    this.imu.setYawAxis(IMUAxis.kY);
-    double pitch = this.imu.getAngle();
+    double yaw = this.imu.getAngle(yawAxis) % 360.0;
+    double roll = this.imu.getAngle(rollAxis) % 360.0;
+    double pitch = this.imu.getAngle(pitchAxis) % 360.0;
     
 
     // To be level:
@@ -50,6 +54,21 @@ public class RobotLeveler extends SubsystemBase {
     // If ROLL > 0   AND ROLL < 45,  turn counter-clockwise
     // If PITCH > 0   AND PITCH < 45,  move forward
     // If PICTH < 360 AND PITCH > 315, move backward
-    return false;
+
+    if (roll < 360.0 && roll > 315.0) {
+      return LevelMovement.RIGHT;
+    } else if (roll > 0.0 && roll < 45.0) {
+      return LevelMovement.LEFT;
+    } else if (pitch > 0.0 && pitch < 45.0) {
+      return LevelMovement.FORWARD;
+    } else if (pitch < 360.0 && pitch > 315.0) {
+      return LevelMovement.REVERSE;
+    }
+
+    return LevelMovement.STOP;
+  }
+
+  public boolean isLevel() {
+    return this.getLevelMovement() == LevelMovement.STOP;
   }
 }
